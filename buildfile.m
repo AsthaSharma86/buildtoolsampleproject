3f76f0752942aefb207de706014ea8f88a37e2d0
asthas <asthas@mathworks.com>
2022-04-12 12:22:14 -0400

    Added compile task.

--- buildfile.m
+++ buildfile.m
@@ -5,9 +5,7 @@ plan = buildplan();
 % Add a task to run your tests by assigning a task object to the plan with a task action
 plan("test") = Task(Actions=@test,Dependencies="compile");
 
-% Add a task to package the contents of the project source folder and declare that it requires the test task
-plan("package") = Task(Actions=@package,Dependencies="test");
-
+% Add a compile task to copile mex file
 plan("compile") = Task(Actions=@compile);
 
 % Set the default tasks on the plan.
@@ -18,11 +16,6 @@ end
 function test(~)
 results = runtests("tests",OutputDetail=1);
 assertSuccess(results);
-end
-
-% Implement the package task action as a local function in the build file
-function package(~)
-zip("tests","*","C:\Users\asthas\OneDrive - MathWorks\Documents\CI\buildtoolsampleproject\tests");
 end
 
 % Implement the task to compile the mex file
