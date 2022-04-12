5e61de30408700737b0c76b9c56863d5d733c75c
asthas <asthas@mathworks.com>
2022-04-12 12:16:59 -0400

    Updated the test verification condition.

--- tests/testyprime.m
+++
@@ -1,8 +0,0 @@
-classdef testyprime < matlab.unittest.TestCase
-    methods(Test)
-        % Test methods
-        function testInput(testCase)
-            verifyError(testCase,@() yprime([8 9 0 7],[0 0],[9 8 7 7]),'MATLAB:TooManyInputs');
-        end
-    end
-end
